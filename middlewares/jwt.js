3ee9b0164fd8eecc3fb6a5b5456bc57fa9401827
eibrahimli <ibrahimlielvir@gmail.com>
2021-06-19 20:11:11 +0400

    last commit

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -12,6 +12,6 @@ module.exports = (req,res,next) => {
         req.user = verified
         next()
     } catch(err) {
-        res.status(400).send('Invalid token')
+        res.status(400).send('Invalid token!')
     }
 }
